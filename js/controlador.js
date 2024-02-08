3e4c2acb88a17a4c44d7f0169e5989fd1552f023
Esther López <estherlopez5577@gmail.com>
2024-02-07 20:01:21 -0600

    Estilos, contador de caracteres

--- js/controlador.js
+++ js/controlador.js
@@ -1,14 +1,29 @@
 let mensaje='';
+let palabra='caracter';
 
-
-
-
+mensaje = document.getElementById('ingreso-mensaje');
+const contador = document.getElementById('contador-caracteres');
+mensaje.addEventListener('input', function(e) {
+    const target = e.target;
+    const longitudAct = target.value.length;
+    if(longitudAct!=1){
+        palabra='caracteres';
+        contador.innerHTML = `Mensaje de ${longitudAct} ${palabra}`;
+    }else{
+        palabra='caracter';
+        contador.innerHTML = `Mensaje de ${longitudAct} ${palabra}`;
+    }
+    
+});
 
 
 function encriptarMensaje() {
-    mensaje=document.getElementById("mensaje").value;
+    mensaje=document.getElementById("ingreso-mensaje").value;
 
     console.log(mensaje);
 
     
 }
+
+
+
